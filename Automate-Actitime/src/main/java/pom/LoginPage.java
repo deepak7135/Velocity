b20b2afa83c6f033f7d2520aff1dc546af5901b8
deepak7135 <98942177+deepak7135@users.noreply.github.com>
2022-02-04 12:52:06 +0530

    Update LoginPage.java

--- Automate-Actitime/src/main/java/pom/LoginPage.java
+++ Automate-Actitime/src/main/java/pom/LoginPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-	
+	//some changes into GitHub
 	private WebDriver driver;  
 	
 
